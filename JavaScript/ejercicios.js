5c5f6d75de4a988294add8940f3975072908d686
Oscar AC <osarcu.tech@gmail.com>
2025-09-01 10:36:24 +0200

    First Append W

---
+++ JavaScript/ejercicios.js
@@ -0,0 +1,116 @@
+
+/*
+Ejercicios de Condicionales
+Bloque para practicar If/Else y Ternarias:
+
+Bloque para practicar con Switch:
+• Muestra en qué estación del año nos encontramos dependiendo del valor de una variable "mes".
+• Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior.
+• Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma.
+*/
+
+/*
+Ejercicios de Estructuras de Datos (Arrays, Sets y Maps)
+• Crea un set que almacene cinco libros.
+• Crea un mapa que asocie el número del mes a su nombre.
+• Comprueba si el mes número cinco existe en el map e imprime su valor.
+• Añade al mapa una clave con un array que almacene los meses de verano. !!!
+• Crea un array, transfórmalo a un set y almacénalo en un map. !!!
+*/
+
+/*
+Ejercicios de Bucles (Loops)
+• Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10.
+*/
+
+/*
+Ejercicios de Funciones
+• Crea una función que reciba dos números y devuelva su suma.
+• Crea una función que reciba un array de números y devuelva el mayor de ellos.
+• Crea una función que reciba un String y devuelva el número de vocales que contiene.
+• Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas.
+• Crea una función que reciba un número y devuelva true si es primo y false en caso contrario.
+• Crea una función que reciba arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos.
+• Crea una función que reciba un array de números y devuelva la suma de todos los números pares.
+• Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado.
+• Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso.
+• Crea una función que calcule el factorial de un número.
+    ◦ Nota: Explora diferentes sintaxis de funciones para resolver los ejercicios.
+*/
+
+/*
+Ejercicios de Objetos
+• Crea un objeto con tres propiedades.
+• Accede y muestra su valor.
+• Agrega una nueva propiedad.
+• Elimina una de las tres primeras propiedades.
+• Agrega una función e invócala.
+*/
+
+/*Ejercicios de Desestructuración y Propagación (Destructuring and Spreading)
+• Usa desestructuración para extraer los dos primeros elementos de un array.
+• Usa desestructuración de un array y asigna un valor predeterminado a una variable.
+• Usa desestructuración para extraer dos propiedades de un objeto.
+• Usa desestructuración para extraer dos propiedades de un objeto y asígnales con nombres diferentes.
+• Usa desestructuración para extraer dos propiedades de un objeto anidado.
+• Usa propagación para combinar dos arrays en uno nuevo.
+• Usa propagación para crear una copia de un array.
+• Usa propagación para combinar dos objetos en uno nuevo.
+• Usa propagación para crear una copia de un objeto.
+• Combina desestructuración y propagación.
+*/
+
+/*
+Ejercicios de Clases
+• Crea una clase que reciba dos propiedades.
+• Añade un método a la clase que utiliza las propiedades.
+• Muestra los valores de las propiedades e invoca la función.
+• Añade un método estático a la primera clase.
+• Haz uso del método estático.
+• Crea una clase que haga uso de herencia.
+• Crea una clase que haga uso de getters y de setters.
+• Modifica la clase con getters y setters para que use propiedades privadas.
+• Utiliza los get y los set y muestra sus valores.
+• Sobreescribe un método de una clase que utilice herencia.
+*/
+
+/*
+Ejercicios de Manejo de Errores
+• Captura una excepción utilizando try-catch.
+• Captura una excepción utilizando try-catch y finally.
+• Lanza una excepción genérica.
+• Crea una excepción personalizada.
+• Lanza una excepción personalizada.
+• Lanza varias excepciones según una lógica.
+• Captura varias excepciones en un mismo try-catch.
+• Crea un bucle que intente transformar a float cada valor y capture y muestre los errores.
+• Crea una función que verifique si un objeto tiene una propiedad específica y lance una excepción personalizada.
+• Crea una función que realice reintentos en caso de error hasta un máximo de 10.
+*/
+
+/*
+Ejercicios de Métodos de Consola (Console Methods)
+• Crea una función que utilice console.error correctamente.
+• Crea una función que utilice console.warn correctamente.
+• Crea una función que utilice console.info correctamente.
+• Utiliza console.table.
+• Utiliza console.group.
+• Utiliza console.time.
+• Valida con console.assert si un número es positivo.
+• Utiliza console.count.
+• Utiliza console.trace.
+• Utiliza console.clear.
+*/
+/*
+Ejercicios de Módulos
+• Exporta una función.
+• Exporta una constante.
+• Exporta una clase.
+• Importa una función.
+• Importa una constante.
+• Importa una clase.
+• Exporta una función, una constante y una clase por defecto.
+• Importa una función, una constante y una clase por defecto.
+• Exporta una función, una constante y una clase desde una carpeta.
+• Importa una función, una constante y una clase desde un directorio diferente al anterior (si se puede o en caso de que lo permita).
+*/
